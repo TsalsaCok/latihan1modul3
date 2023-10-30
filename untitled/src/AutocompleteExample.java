0ea06502f7207dd6a50271fc7b9f6604ee07fac5
Sandy Tsalsa Fanany <sandytsalsa@gmail.com>
2023-10-31 02:54:02 +0700

    first commit

---
+++ untitled/src/AutocompleteExample.java
@@ -0,0 +1,7 @@
+public class AutocompleteExample {
+    public static void main(String[] args) {
+        String exampleString = "Hello, World!";
+        // Implementasikan fitur autocomplete di sini
+        System.out.println(exampleString);
+    }
+}
